4d81ed26b3ef53bb10a64f75d53c018c84ba5c0c
Yankers <vovarojko@.com>
2021-12-22 00:04:10 +0200

    added 4.6

--- 4.5 Конструкторы, создание объектов через new/task.js
+++ 4.5 Конструкторы, создание объектов через new/task.js
@@ -49,31 +49,60 @@
 // mul() возвращает произведение введённых свойств.
 //     Например:
 
-function Calculator() {
+// function Calculator() {
+//
+//
+//     this.sum = function()
+//     {
+//       return this.a + this.b
+//     };
+//     this.mul = function()
+//     {
+//         return this.a * this.b;
+//     };
+//
+//     this.read = function()
+//     {
+//         this.a = +prompt('a?', '');
+//         this.b = +prompt('b?', "");
+//     };
+// }
+//
+// let calculator = new Calculator();
+//
+// calculator.read();
+//
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
 
 
-    this.sum = function()
-    {
-      return this.a + this.b
-    };
-    this.mul = function()
-    {
-        return this.a * this.b;
-    };
 
-    this.read = function()
-    {
-        this.a = +prompt('a?', '');
-        this.b = +prompt('b?', "");
-    };
+// Создаём Accumulator
+// важность: 5
+// Напишите функцию-конструктор Accumulator(startingValue).
+//
+//     Объект, который она создаёт, должен уметь следующее:
+//
+//     Хранить «текущее значение» в свойстве value. Начальное значение устанавливается в аргументе конструктора startingValue.
+//     Метод read() использует prompt для получения числа и прибавляет его к свойству value.
+//     Таким образом, свойство value является текущей суммой всего, что ввёл пользователь при вызовах метода read(), с учётом начального значения startingValue.
+//
+//     Ниже вы можете посмотреть работу кода:
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function() {
+      this.value += +prompt('Сколько нужно добавить', 0);
+  };
 }
 
-let calculator = new Calculator();
-
-calculator.read();
-
-alert( "Sum=" + calculator.sum() );
-alert( "Mul=" + calculator.mul() );
 
 
 
+    let accumulator = new Accumulator(1); // начальное значение 1
+
+accumulator.read(); // прибавит ввод prompt к текущему значению
+accumulator.read(); // прибавит ввод prompt к текущему значению
+
+alert(accumulator.value); // выведет сумму этих значений
